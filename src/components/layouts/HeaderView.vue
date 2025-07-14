4020c177f24394070a03a7eb76621aa90113793b
donggeon Na <ndmb2012@gmail.com>
2025-07-15 01:43:35 +0900

    design: 헤더, 푸터, 로그인 디자인

--- src/components/layouts/HeaderView.vue
+++ src/components/layouts/HeaderView.vue
@@ -2,11 +2,9 @@
   <nav id="nav" class="flex bg-green-800 py-5 px-5 text-green-50 text-base">
     <div class="flex gap-5 mx-auto">
       <RouterLink :to="{ name: 'home' }">홈</RouterLink>
+      <RouterLink :to="{ name: 'login' }">login test</RouterLink>
       <RouterLink :to="{ name: 'board/detail', params: { id: 1 } }">board/detail test</RouterLink>
     </div>
-    <!-- <div v-if="authStore.isLogin">
-      <button @click="handleLogout">로그아웃</button>
-    </div> -->
   </nav>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,15 @@ import HomePage from '../pages/HomePage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: '/', name: 'home', component: HomePage }, ...boardRoutes],
+  routes: [
+    { path: '/', name: 'home', component: HomePage },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../pages/auth/LoginView.vue'),
+    },
+    ...boardRoutes,
+  ],
 });
 
 export default router;
